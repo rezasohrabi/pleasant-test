230dcbc8c5255dea01487d7d5f8d7f39c740377e
Reza sohrbi <rezasohrabi1995@yahoo.com>
2021-10-04 09:06:23 +0330

    Add css transition group package

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from './components/Header';
 import Login from './components/Login';
 import SaveToLocalstorage from './components/SaveToLocalstorage';
+import Carousel from './Carousel';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <SaveToLocalstorage />
       <hr />
       <Login />
+      <Carousel />
     </div>
   );
 }
