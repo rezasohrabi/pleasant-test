7b2f3f5e5571d84fdf172446c66f397122a0bd9f
= <rezasohrabi1995@yahoo.com>
2021-10-20 00:53:05 +0330

    refactor app, add bulma

--- razzle-ssr/src/App.js
+++ razzle-ssr/src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Home from './Home';
-import './App.css';
+import './App.module.scss';
+import 'bulma/css/bulma.min.css';
+
 
 const App = () => (
   <Switch>
