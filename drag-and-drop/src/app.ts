76b8defb96f4bb5c0af85f8e0d94433207f1fb54
rezasohrabi <rezasohrabi1995@yahoo.com>
2021-12-22 15:25:26 +0330

    chore: attached form section to page

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -1,1 +1,24 @@
-console.log('this is log');
+class TodoForm {
+  templateElement: HTMLTemplateElement;
+  appElement: HTMLDivElement;
+  element: HTMLElement;
+
+  constructor() {
+    this.templateElement = document.getElementById(
+      'add-todo-form'
+    ) as HTMLTemplateElement;
+    this.appElement = document.getElementById('app') as HTMLDivElement;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = importedNode.firstElementChild as HTMLElement;
+    this.attach();
+  }
+
+  attach() {
+    this.appElement.insertAdjacentElement('afterbegin', this.element);
+  }
+}
+
+new TodoForm();
