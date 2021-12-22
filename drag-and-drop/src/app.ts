f51aef37f02c0c17bedc89af6a2416f591a7f056
rezasohrabi <rezasohrabi1995@yahoo.com>
2021-12-22 18:17:25 +0330

    chore: added todo form submit handler

--- drag-and-drop/src/app.ts
+++ drag-and-drop/src/app.ts
@@ -2,6 +2,9 @@ class TodoForm {
   templateElement: HTMLTemplateElement;
   appElement: HTMLDivElement;
   element: HTMLElement;
+  usernameInputElement: HTMLInputElement;
+  todoInputElement: HTMLInputElement;
+  completedCheckboxElement: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
@@ -13,7 +16,27 @@ class TodoForm {
       true
     );
     this.element = importedNode.firstElementChild as HTMLElement;
+    this.element.id = 'add-todo-input';
+    this.usernameInputElement = this.element.querySelector(
+      '#username-input'
+    ) as HTMLInputElement;
+    this.todoInputElement = this.element.querySelector(
+      '#todo-input'
+    ) as HTMLInputElement;
+    this.completedCheckboxElement = this.element.querySelector(
+      '#is-completed-input'
+    ) as HTMLInputElement;
+    this.configureSubmit();
     this.attach();
+  }
+
+  submitHandler(event: Event) {
+    event.preventDefault();
+    console.log(this.usernameInputElement.value, this.todoInputElement.value);
+  }
+
+  configureSubmit() {
+    this.element.addEventListener('submit', this.submitHandler.bind(this));
   }
 
   attach() {
